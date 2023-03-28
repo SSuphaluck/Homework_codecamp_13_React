39e66dbaf1b810002e07e1d00d102cacba737e5d
SSuphaluck <hibara.and.conun@gmail.com>
2023-03-28 13:19:38 +0700

    forms

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState('');
+  const [monthOfBirth, setMonthOfBirth] = useState('');
+  const [input, setInput] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form>
+      <p>
+        Name :{' '}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </p>
+      <p>
+        Gender :{' '}
+        <input
+          type="radio"
+          name="gender"
+          value="man"
+          checked={gender === 'man'}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        <label htmlFor="gender">Man</label>
+        <input
+          type="radio"
+          name="gender"
+          value="women"
+          checked={gender === 'women'}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        <label htmlFor="gender">Women</label>
+        <input
+          type="radio"
+          name="gender"
+          value="others"
+          checked={gender === 'others'}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        <label htmlFor="gender">Others</label>
+      </p>
+      <p>
+        Month of Birth :{' '}
+        <select
+          name="monthOfBirth"
+          value={monthOfBirth}
+          onChange={(e) => setMonthOfBirth(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="">select</option>
+          <option value="January">January</option>
+          <option value="February">February</option>
+          <option value="March">March</option>
+          <option value="April">April</option>
+          <option value="May">May</option>
+          <option value="June">June</option>
+          <option value="July">July</option>
+          <option value="August">August</option>
+          <option value="September">September</option>
+          <option value="October">October</option>
+          <option value="November">November</option>
+          <option value="December">December</option>
+        </select>
+      </p>
+      <p>
+        Your picture :{' '}
+        <input
+          type="file"
+          onChange={(event) => setInput(event.target.value)}
+        />
+      </p>
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log({ name, gender, monthOfBirth, input });
+        }}
+      >
+        Submit
+      </button>
+    </form>
   );
 }
 
